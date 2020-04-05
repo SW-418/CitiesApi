072d0ee10698592b9b8dee077eb7c0ace5be4dd0
Sam Wells <samwells95@gmail.com>
2020-04-05 13:32:55 +0100

    Add xml support

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -11,7 +12,12 @@ namespace CityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc(option =>
+            {
+                option.EnableEndpointRouting = false;
+                option.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+               
+            });
                 //.AddJsonOptions(o =>
                 //{
                 //    o.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase; //Ensure use of camelCase
